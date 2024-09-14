18ed22f9a3ee2dcdf0b59fdfbf00c1507eb34a2a
Sergei Vakunov <bakunobu@gmail.com>
2024-09-14 13:05:27 +0300

    first page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,23 @@
 from app import app
+import datetime as dt
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {
+    'username': 'Sergei Vakunov',
+    'login': 'bakunobu',
+    'date': dt.datetime.now(),
+    'author': 'Р. Адамс',
+    'title': 'Обитатели холмов'
+    }
+  return f'''
+    <html>
+      <head>
+        <title>Home Page - Microblog</title>
+      </head>
+      <body>
+        <h1>{user.get('username')} @{user.get('login')}</h1>
+        <h2>Прочитано: {user.get('author')}.{user.get('title')}.</h2>
+        <h2>{user.get('date')}</h2>
+      </body>
+    </html>'''
