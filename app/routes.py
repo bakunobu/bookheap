40e4d3c4761ab231a9f660a7fa6c09b4e7e2624e
Sergei Vakunov <bakunobu@gmail.com>
2024-09-16 06:50:26 +0300

    multiple posts

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,25 @@ import datetime as dt
 @app.route('/')
 @app.route('/index')
 def index():
-  user = {
+  user = {'username': 'Sergei Vakunov'}
+  posts = [
+    {
+    'username': 'Sergei Vakunov',
+    'login': 'bakunobu',
+    'date': dt.datetime.now(),
+    'author': 'В. Ян',
+    'title': 'Юность полководца'},
+
+    {
     'username': 'Sergei Vakunov',
     'login': 'bakunobu',
     'date': dt.datetime.now(),
     'author': 'Р. Адамс',
     'title': 'Обитатели холмов'
     }
+  ]
+    
   return render_template('index.html',
                          title='Home',
-                         user=user)
+                         user=user,
+                         posts=posts)
