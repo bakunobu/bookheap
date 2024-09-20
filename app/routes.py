2df17084091e62868fea41ae4ef677a384311075
Sergei Vakunov <bakunobu@gmail.com>
2024-09-20 06:40:27 +0300

    login form added to routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 import datetime as dt
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -26,3 +29,9 @@ def index():
                          title='Home',
                          user=user,
                          posts=posts)
+
+
+@app.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html', title='Sign In', form=form)
