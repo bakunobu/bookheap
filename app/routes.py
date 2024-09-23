14795e8eb31ed4c02d85038b48dd7faa8aea428d
Sergei Vakunov <bakunobu@gmail.com>
2024-09-23 06:45:06 +0300

    logout added

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 import datetime as dt
 from flask_login import current_user, login_user
+from flask_login import logout_user
 import sqlalchemy as sa
 from app import db
 from app.models import User
@@ -69,3 +70,8 @@ def login():
     login_user(user, remember=form.remember_me.data)
     return redirect(url_for('index'))
   return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
