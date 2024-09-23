bed798b251acf8fb78b330462a17fe10ee9c107c
Sergei Vakunov <bakunobu@gmail.com>
2024-09-23 06:41:11 +0300

    login support added

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,10 @@ from app import app
 from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 import datetime as dt
+from flask_login import current_user, login_user
+import sqlalchemy as sa
+from app import db
+from app.models import User
 
 
 @app.route('/')
@@ -48,4 +52,20 @@ def login():
       form.username.data, form.remember_me.data))
     return redirect(url_for('index'))
 
+  return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = db.session.scalar(
+      sa.select(User).where(User.username == form.username.data))
+    if user is None or not user.check_password(form.password.data):
+      flash('Invalid username or password')
+      return redirect(url_for('login'))
+    login_user(user, remember=form.remember_me.data)
+    return redirect(url_for('index'))
   return render_template('login.html', title='Sign In', form=form)
