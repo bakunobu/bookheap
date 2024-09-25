e8cd4511a2fe32c68bd52170306c5ce4ce35d6ab
Sergei Vakunov <bakunobu@gmail.com>
2024-09-25 06:49:46 +0300

    new login logic

--- app/routes.py
+++ app/routes.py
@@ -4,13 +4,17 @@ from app.forms import LoginForm
 import datetime as dt
 from flask_login import current_user, login_user
 from flask_login import logout_user
+from flask_login import login_required
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from flask import request
+from urllib.parse import urlsplit
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
   user = {'username': 'Sergei Vakunov'}
   posts = [
@@ -51,14 +55,16 @@ def login():
     return redirect(url_for('index'))
   form = LoginForm()
   if form.validate_on_submit():
-    user = db.session.scalar(
-      sa.select(User).where(User.username == form.username.data))
-    if user is None or not user.check_password(form.password.data):
-      flash('Invalid username or password')
-      return redirect(url_for('login'))
-    login_user(user, remember=form.remember_me.data)
-    return redirect(url_for('index'))
-  return render_template('login.html', title='Sign In', form=form)
+      user = db.session.scalar(
+          sa.select(User).where(User.username == form.username.data))
+      if user is None or not user.check_password(form.password.data):
+          flash('Invalid username or password')
+          return redirect(url_for('login'))
+      login_user(user, remember=form.remember_me.data)
+      next_page = request.args.get('next')
+      if not next_page or urlsplit(next_page).netloc != '':
+          next_page = url_for('index')
+      return redirect(next_page)
 
 @app.route('/logout')
 def logout():
